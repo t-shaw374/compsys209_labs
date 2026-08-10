accfc5263e2089b17c917c425c37af600684fc6a
t-shaw374 <tsha374@aucklanduni.ac.nz>
2026-08-10 17:03:00 +1200

    lab 2 part 4.7

--- Lab2_UART/microchipStudioTesting/microchipStudioTesting/main.c
+++ Lab2_UART/microchipStudioTesting/microchipStudioTesting/main.c
@@ -14,8 +14,7 @@
 
 #define BAUD 9600
 #define UBRR_VAL ((F_CPU / (16UL * BAUD)) - 1)
-#define SEND_DELAY 500 //ms
-#define SEND_NUM 345
+#define SEND_DELAY 5000 //ms
 
 
 
@@ -31,35 +30,49 @@ void usart_transmit(uint8_t data) {
 	UDR0 = data;
 }
 
+void send_3_digit(uint16_t num) {
+	uint8_t num_chars[] = {(num / 100) % 10, (num / 10) % 10, num % 10};
+	for(uint8_t i = 0; i < 3; i++) {
+		usart_transmit(num_chars[i] + 48);
+	}
+}
+
+void send_commaspace() {
+	usart_transmit(44);
+	usart_transmit(32);
+}
+
 int main(void)
 {
 	usart_init(UBRR_VAL);
 	
-	//uint8_t position = 0;
-	//uint16_t found_primes[62];
-	//for(uint16_t i = 2; i <= 300; i++) {
-		//uint8_t is_prime = 1;
-		//for(uint16_t j = 2; j < i; j++) {
-			//if(i % j == 0) {
-				//is_prime = 0;
-			//}
-		//}
-		//if(is_prime) {
-			//found_primes[position] = i;
-			//position++;
-		//}
-	//}
-	//uint16_t debug_check = found_primes[0];  
+	uint8_t position = 0;
+	uint16_t found_primes[62];
+	for(uint16_t i = 2; i <= 300; i++) {
+		uint8_t is_prime = 1;
+		for(uint16_t j = 2; j < i; j++) {
+			if(i % j == 0) {
+				is_prime = 0;
+			}
+		}
+		if(is_prime) {
+			found_primes[position] = i;
+			position++;
+		}
+	}
+	
     /* Replace with your application code */
 	
 	
     while (1) 
     {
-			
-		uint8_t num_chars[] = {(SEND_NUM / 100) % 10, (SEND_NUM / 10) % 10, SEND_NUM % 10};
-		for(uint8_t i = 0; i < 3; i++) {
-			usart_transmit(num_chars[i] + 48);
+		for(uint16_t vro = 0; vro < 62; vro++) {
+			send_3_digit(found_primes[vro]);
+			if (vro < 61) {
+				send_commaspace();
+			}
 		}
+
 		
 		_delay_ms(SEND_DELAY);
 		
