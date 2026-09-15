2e4f8a1b3b274e7680c7014b68037ae552f11350
t-shaw374 <tsha374@aucklanduni.ac.nz>
2026-09-15 14:00:25 +1200

    part 4 completed (note digital logbook not filled in yet)

--- Lab4_ADC/lab4_microchip/Lab4/Lab4/main.c
+++ Lab4_ADC/lab4_microchip/Lab4/Lab4/main.c
@@ -9,19 +9,45 @@
 
 #include <avr/io.h>
 #include "adc.h"
+#include "uart.h"
 #include "common.h"
 #include <util/delay.h>
 
+#define BAUD 9600
+#define UBRR_VAL ((F_CPU / ((uint32_t)16 * BAUD)) - 1)
+#define ADC_DELAY 0.1 //ms
+#define NUM_SAMPLES 100
+
+
 int main(void)
 {
+	usart_init(UBRR_VAL);
 	adc_init();
-    uint16_t adc_value;
-	uint16_t value_mv;
+    //uint16_t adc_value;
+	//uint16_t value_mv;
+	uint16_t adc0_values[NUM_SAMPLES];
+	uint16_t adc1_values[NUM_SAMPLES];
+	
+	for (uint8_t i = 0; i<NUM_SAMPLES; i++) {
+		adc0_values[i] = adc_convert_mV(adc_read(0));
+		adc1_values[i] = adc_convert_mV(adc_read(1));
+		
+	}
+		
+	for (uint8_t i = 0; i<NUM_SAMPLES;i++) {
+		send_mV_value(adc0_values[i]);
+		usart_transmit(' ');
+		send_mV_value(adc1_values[i]);
+		usart_transmit('\r');
+		usart_transmit('\n');
+	}
+	
     while (1) 
     {
-		adc_value = adc_read(2); // read ADC2
-		value_mv = adc_convert_mV(adc_value);
-		_delay_ms(1);
+		//adc_value = adc_read(2); // read ADC2
+		//value_mv = adc_convert_mV(adc_value);
+		//send_mV_value(value_mv);
+		//_delay_ms(1000);
     }
 }
 
---
+++ Lab4_ADC/lab4_microchip/Lab4/Lab4/uart.c
@@ -0,0 +1,29 @@
+/*
+ * uart.c
+ *
+ * Created: 15/09/2026 1:11:27 pm
+ *  Author: thoma
+ */ 
+
+#include <avr/io.h>
+#include "uart.h"
+
+void usart_init(uint16_t ubrr) {
+	UCSR0B = (1 << TXEN0);
+	UCSR0C = (1 << UCSZ01 | 1 << UCSZ00);
+	UBRR0L = (uint8_t)(ubrr);
+	UBRR0H = (uint8_t)(ubrr >> 8);
+}
+
+void usart_transmit(uint8_t data) {
+	while (!(UCSR0A & (1 << UDRE0))) {}
+	UDR0 = data;
+	
+}
+
+void send_mV_value(uint16_t num) {
+	uint8_t num_chars[] = {(num / 1000) % 10, (num / 100) % 10, (num / 10) % 10, num % 10};
+	for(uint8_t i = 0; i < 4; i++) {
+		usart_transmit(num_chars[i] + 48);
+	}
+}
